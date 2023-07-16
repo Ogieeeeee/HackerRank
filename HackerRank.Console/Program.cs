a9b2dbca5cd17963c94a59d6660df49fcc5463e6
Oguzhan K <o.kelleci@live.nl>
2023-07-16 16:22:08 +0200

    Print the exercise with description and new lines

--- HackerRank.Console/Program.cs
+++ HackerRank.Console/Program.cs
@@ -1,5 +1,6 @@
 //Warmup
 
+//SockMerchant
 List<int> ar = new() { 10, 20, 20, 10, 10, 30, 50, 10, 20 };
-Console.WriteLine(WarmUpChallanges.SockMerchant(ar.Count, ar));
+Console.WriteLine("Socksmerchant \n");
 Console.WriteLine(WarmUpChallenges.SockMerchant(ar.Count, ar) + '\n');
